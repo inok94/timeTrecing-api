27872416210c87e5b0ef75533e2f635e88c5e5b6
inok94 <rodiknikolaev@yadndex.ru>
2018-04-02 12:47:58 +0300

    Update model User

--- app/User.php
+++ app/User.php
@@ -26,4 +26,59 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function client()
+    {
+        return $this->belongsToMany(
+            Client::class,
+            'employeeсlients',
+            'id',
+            'client_id'
+        )
+            ->withTimestamps();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function clients()
+    {
+        return $this->belongsToMany(
+            Project::class,
+            'employeeсlients',
+            'user_id',
+            'client_id'
+        )
+            ->withTimestamps();
+    }
+
+    public function project()
+    {
+        return $this->belongsToMany(
+            Project::class,
+            'project_employees',
+            'user_id',
+            'project_id'
+        )
+            ->withTimestamps();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function timeInWorkstation()
+    {
+        return $this->hasMany('App\TimeEmployee');
+    }
+
+    public function generateToken()
+    {
+        $this->remember_token = str_random(60);
+        $this->save();
+
+        return $this->remember_token;
+    }
 }
